65122e71f50d4deaa79c2ef19c5d7b7c7d68c150
richardimaoka <richard.s.imaoka@gmail.com>
2025-08-31 14:43:19 +0900

    Intrinsic and extrinsic sized rectangles

--- src/types.ts
+++ src/types.ts
@@ -2,9 +2,25 @@ type CSSGridContainer = {
   // To be defined
 };
 
+enum SizingType {
+  INTRINSIC = 'intrinsic',
+  EXTRINSIC = 'extrinsic'
+}
+
+type IntrinsicSizing = {
+  type: SizingType.INTRINSIC;
+  valuePx: number;
+};
+
+type ExtrinsicSizing = {
+  type: SizingType.EXTRINSIC;
+};
+
+type Sizing = IntrinsicSizing | ExtrinsicSizing;
+
 type PlaceholderRect = {
-  widthPx: number;
-  heightPx: number;
+  width: Sizing;
+  height: Sizing;
 };
 
 type TextContent = {

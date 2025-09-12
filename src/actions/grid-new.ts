989ac767ef5654c3c29b1b4f34517b37782d2d14
richardimaoka <richard.s.imaoka@gmail.com>
2025-09-12 13:45:36 +0900

    test bodies

--- src/actions/grid-new.ts
+++ src/actions/grid-new.ts
@@ -7,8 +7,10 @@ import {
 } from "../types";
 import {
   createPlaceholderRectNew,
-  widthCenter,
   heightCenter,
+  stretchHeight,
+  stretchWidth,
+  widthCenter,
 } from "./rect-new";
 
 export const hugWidth: HugWidth = {
@@ -24,9 +26,14 @@ export const hugHeight: HugHeight = {
  * @param rect
  */
 function encloseInGrid(rect: PlaceholderRectNew): CSSGridContainerNew {
+  const gridWidth =
+    rect.width.type === SizingTypeNew.STRETCH ? stretchWidth : hugWidth;
+  const gridHeight =
+    rect.height.type === SizingTypeNew.STRETCH ? stretchHeight : hugHeight;
+
   return {
-    width: hugWidth,
-    height: hugHeight,
+    width: gridWidth,
+    height: gridHeight,
     children: [rect],
   };
 }
@@ -35,7 +42,7 @@ if (import.meta.vitest) {
   const { describe, it, expect } = import.meta.vitest;
 
   describe("encloseInGrid", () => {
-    it("should enclose a placeholder rect with fixed width and height with hug sizing", () => {
+    it("should enclose a fixed width/height placeholder rect into a hug width/height CSS grid", () => {
       const rect = createPlaceholderRectNew(
         widthCenter(100),
         heightCenter(100)
@@ -44,6 +51,36 @@ if (import.meta.vitest) {
       expect(grid).toEqual({
         width: hugWidth,
         height: hugHeight,
+        children: [rect],
+      });
+    });
+
+    it("should enclose a stretch width/fixed height placeholder rect into a stretch width/hug height CSS grid", () => {
+      const rect = createPlaceholderRectNew(stretchWidth, heightCenter(100));
+      const grid = encloseInGrid(rect);
+      expect(grid).toEqual({
+        width: stretchWidth,
+        height: hugHeight,
+        children: [rect],
+      });
+    });
+
+    it("should enclose a fixed width/stretch height placeholder rect into a hug width/stretch height CSS grid", () => {
+      const rect = createPlaceholderRectNew(widthCenter(100), stretchHeight);
+      const grid = encloseInGrid(rect);
+      expect(grid).toEqual({
+        width: hugWidth,
+        height: stretchHeight,
+        children: [rect],
+      });
+    });
+
+    it("should enclose a stretch width/height placeholder rect into a stretch width/height CSS grid", () => {
+      const rect = createPlaceholderRectNew(stretchWidth, stretchHeight);
+      const grid = encloseInGrid(rect);
+      expect(grid).toEqual({
+        width: stretchWidth,
+        height: stretchHeight,
         children: [rect],
       });
     });
